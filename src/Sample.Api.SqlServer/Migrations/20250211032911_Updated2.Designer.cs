90580e33857ea97655f4baf9cb20975de84cb70b
Juan Pablo Ibanez <ing.juanpablo@gmail.com>
2025-02-11 17:56:29 +1000

    Upgrade to 8.3.4. Add missing migration. Fix SampleRequest

--- src/Sample.Api.SqlServer/Migrations/20250211032911_Updated2.Designer.cs
+++ src/Sample.Api.SqlServer/Migrations/20250211032911_Updated2.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Sample.Components;
 
@@ -11,9 +12,11 @@ using Sample.Components;
 namespace Sample.Api.SqlServer.Migrations
 {
     [DbContext(typeof(SampleDbContext))]
-    partial class SampleDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250211032911_Updated2")]
+    partial class Updated2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
