a4155f88ba36ede8ddeb5c3e49dbef97ee0135d6
Juan Pablo Ibanez <ing.juanpablo@gmail.com>
2025-02-11 17:56:29 +1000

    Upgrade to 8.3.4. Add missing migration. Fix SampleRequest

---
+++ src/Sample.Api.SqlServer/Migrations/20250211032911_Updated2.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sample.Api.SqlServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class Updated2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "job_properties",
+                schema: "sample",
+                table: "job_saga",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "job_properties",
+                schema: "sample",
+                table: "job_saga");
+        }
+    }
+}
